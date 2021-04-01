089183b68f15563f459c4bcebaf4c1c8025ca2a1
poegva <poegva@mail.ru>
2021-04-01 18:08:00 +0300

    Redundant test contracts removed

--- contracts/interfaces/ITokenManager.sol
+++ contracts/interfaces/ITokenManager.sol
@@ -3,10 +3,6 @@ pragma solidity 0.7.3;
 abstract contract ITokenManager {
     function mint(address receiver, uint96 amount) virtual external;
 
-    function issue(uint96 _amount) virtual external;
-
-    function assign(address _receiver, uint96 _amount) virtual external;
-
     function burn(address holder, uint96 amount) virtual external;
 
 }
