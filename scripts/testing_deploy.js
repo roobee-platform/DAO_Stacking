ce02d11bede7438c6924fbeb849934feca48397d
poegva <poegva@mail.ru>
2021-07-05 20:31:41 +0300

    Update testing_deploy.js

--- scripts/testing_deploy.js
+++ scripts/testing_deploy.js
@@ -7,23 +7,48 @@ async function main() {
 
     Token = await ethers.getContractFactory('StandartToken');
     GovernanceToken = await ethers.getContractFactory('GovernanceToken');
-    Governor = await ethers.getContractFactory('GovernorExpress');
+    Governor = await ethers.getContractFactory('GovernorAlpha');
     Staking  = await ethers.getContractFactory('DAOStacking');
+    Timelock = await ethers.getContractFactory('Timelock');
     
-    token = await Token.deploy('Roobee', 'ROOBEE', 18, etherMantissa(100000000).toString());
-    // token = await Token.attach('');
+    token = await Token.attach(''/*ROOBEE ADDRESS*/);
+    console.log(`REACT_APP_ROOBEE_ADDRESS=${token.address}`);
 
     // Initialize contracts
-    govToken = await GovernanceToken.deploy(account.address);
-    governor = await Governor.deploy(address(0), govToken.address, account.address);
-    staking = await Staking.deploy();
-    await staking.init(govToken.address, token.address, govToken.address);
-    await govToken.setMinter(staking.address);
+    govToken = await GovernanceToken.deploy(account.address, {gasLimit: 8000000});
+    console.log(`REACT_APP_GOV_ROOBEE_ADDRESS=${govToken.address}`);
 
-    // Log addresses
-    console.log(`REACT_APP_ROOBEE_ADDRESS=${token.address}`);
-    console.log(`REACRT_APP_GOV_ROOBEE_ADDRESS=${govToken.address}`)
+    timelock = await Timelock.deploy(account.address, 10/*DELAY VALUE*/);
+
+    governor = await Governor.deploy(
+        timelock.address, 
+        govToken.address,
+        account.address,
+        0/*PROPOSAL THRESHOLD*/,
+        0/*QUORUM VOTES*/,
+        0/*VOTING DELAY*/,
+        0/*VOTING PERIOD*/,
+        {gasLimit: 8000000}
+    );
+    console.log(`REACT_APP_GOVERNOR_ADDRESS=${governor.address}`);
+
+    staking = await Staking.deploy({gasLimit: 8000000});
     console.log(`REACT_APP_STAKING_ADDRESS=${staking.address}`)
-    console.log(`REACT_APP_GOVERNOR_ADDRESS=${governor.address}`)
+    //staking = await Staking.attach('0x9DAa0188d537fB17ef265efbd64f1B8a41d4665b');
+    await staking.init(
+        govToken.address, 
+        token.address, 
+        govToken.address,
+        172800/*REWARDS DURATION */, 
+        10/*EXCHANGRE RATE*/, 
+        10/*MAX LOCKS*/,
+        {gasLimit: 8000000}
+    );
+
+    await govToken.setMinter(staking.address, {gasLimit: 8000000});
+
+    console.log("Setup complete");
 }
 
+main()
+ 
